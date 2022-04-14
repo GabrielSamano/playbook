8cbdb91df023847180676d5895a54a9c14c331e1
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-04-14 09:44:51 -0500

    Add files via upload

---
+++ weekly_mission_2/Examples/example_0/example1.js
@@ -0,0 +1,6 @@
+const myObjetc2 = {
+    name: "Gabriel",
+    age: 24
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(myObjetc2)
---
+++ weekly_mission_2/Examples/example_0/example2.js
@@ -0,0 +1,19 @@
+const myObject3 = {
+    name: "Fili",
+    age: 2,
+    nicknames: [
+     "Filis",
+      "Suzie",
+      "Filigonia"
+    ],
+    address: {
+      zip_code: "10100",
+      street: "Dr. Vertiz 12 Benito Juarez",
+      city: "CDMX"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.name)
+  console.log(myObject3["address"])
+  
